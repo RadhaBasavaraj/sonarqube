956e8c2c7112a30ebe67fdbd5d5534db72891f04
SaniaG <autoexprt123@gmail.com>
2017-05-10 07:57:06 -0700

    Adding java file

---
+++ MyClass.java
@@ -0,0 +1,17 @@
+package myPackage;
+
+public class MyClass {
+  int i = 0;
+  final String test=null;
+  final String test1="test1";
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String something = new String("1")+ new String ("2");		
+		System.out.println("Hello World");
+
+	}
+
+}
